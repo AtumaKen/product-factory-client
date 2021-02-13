ad95eb9ebbe4667d499ccc3bf12d30e69d6a8668
AtumaKen <atumaken@gmail.comgit config --global user.name AtumaKengit commitgit statusgit statusgitgit config --global user.email atumaken@gmail.com>
2021-02-13 17:03:08 +0100

    Initial commit

---
+++ src/main/java/com/kenoly/productfactoryclient/ProductFactoryClientApplication.java
@@ -0,0 +1,13 @@
+package com.kenoly.productfactoryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductFactoryClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductFactoryClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kenoly/productfactoryclient/ProductFactoryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenoly.productfactoryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductFactoryClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
