646cd125f45442f583e0ae6ac9a7eeeb77c2bd5e
AtumaKen <atumaken@gmail.comgit config --global user.name AtumaKengit commitgit statusgit statusgitgit config --global user.email atumaken@gmail.com>
2021-02-13 17:27:33 +0100

    added models.

---
+++ src/main/java/com/kenoly/productfactoryclient/model/CustomerDto.java
@@ -0,0 +1,8 @@
+package com.kenoly.productfactoryclient.model;
+
+import java.util.UUID;
+
+public class CustomerDto {
+    private UUID id;
+    private String name;
+}
---
+++ src/main/java/com/kenoly/productfactoryclient/model/ProductDto.java
@@ -0,0 +1,10 @@
+package com.kenoly.productfactoryclient.model;
+
+import java.util.UUID;
+
+public class ProductDto {
+    private UUID id;
+    private String productName;
+    private String productStyle;
+    private Long upc;
+}
